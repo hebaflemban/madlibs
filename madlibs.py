eae948e78e749f02627a9e92c846aa9067613551
Heba <36724067+heba@users.noreply.github.com>
2020-08-16 13:39:16 +0300

    the story without new lines

--- madlibs.py
+++ madlibs.py
@@ -1,11 +1,12 @@
 def main():
+
 	time = input("Enter a number from 1 to 12:")
 	items = input("Enter a noun (plural):")
 	name = input("Enter a name:")
 	scream = input("Enter any sentence:")
 	action = input("Enter a verb:")
 
-	story = " The story goes... \n It was %s o\'clock when I heard a knock at the door.\n I opened the door and there was a box full of %s with a note saying \"From Mr. %s \" \n Just as I closed the door I heard a scream \" %s \" \n I froze in place and all I could do was %s .\"  " %(time, items, name.capitalize(), scream.upper(), action )
+	story = "It was %s o\'clock when I heard a knock at the door. I opened the door and there was a box full of %s with a note saying \"From %s\". Just as I closed the door I heard a scream \"%s\". I froze in place and all I could do was %s.\"  " %(time, items, name.capitalize(), scream.upper(), action )
 
 	print(story)
 
